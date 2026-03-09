734dfac12b749c759896ec8dc637f2ac83f88044
matteoroxis <matteoroxis@gmail.com>
2026-03-09 11:47:42 +0100

    Update with remaining classes

---
+++ src/main/java/it/matteoroxis/reactive_streaming/ReactiveStreamingApplication.java
@@ -0,0 +1,13 @@
+package it.matteoroxis.reactive_streaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveStreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveStreamingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/matteoroxis/reactive_streaming/ReactiveStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package it.matteoroxis.reactive_streaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
